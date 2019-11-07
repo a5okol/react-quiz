d0250c13349f4b95a2e1e0651569d6b48784a57c
Andrew Sokolenko <a5okowork@gmail.com>
2019-11-07 23:16:21 +0200

    Добовили первый компонент Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Layout from './hoc/Layout/Layout'
+import './hoc/Layout/Layout.scss'
 
 function App() {
   return (
-      <div>
-        <h1>Hello React</h1>
-        <h2>Hello React</h2>
-        <h2>Hello React</h2>
-        <h2>Hello React</h2>
+      <Layout>
 
-      </div>
+          <div style={{width: 400, border: '1px solid black'}} className="Layout">
+              <h1>Layout works</h1>
+          </div>
+
+      </Layout>
   );
 }
 
