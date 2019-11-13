2d12beb20bee1f51443fd2a708c90964758b122b
Andrew Sokolenko <a5okowork@gmail.com>
2019-11-13 18:28:39 +0200

    Создал кнопку перемещения меню

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
 
 class Layout extends Component {
+
+    state = {
+      menu: false
+    };
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    };
     render() {
         return (
             <div>
+                <MenuToggle
+                onToggle={this.toggleMenuHandler}
+                isOpen={this.state.menu}
+                />
                 <main>
                     {this.props.children}
                 </main>
