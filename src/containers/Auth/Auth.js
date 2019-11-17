54c55961642b83917c8194b0e1f8502c239e7470
Andrew Sokolenko <a5okowork@gmail.com>
2019-11-17 15:15:49 +0200

    Создание страницы авторизации и компонента текстового поля.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,49 @@
 import React, {Component} from 'react'
+import './Auth.scss'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/input/input'
 
 export default class Auth extends Component {
+
+    loginHandler = () =>{
+
+    };
+
+    registerHandler = () => {
+
+    };
+
+    submitHandler = event => {
+
+    };
+
     render() {
         return(
-        <div>
-            <h1>Auth</h1>
+        <div className={'Auth'}>
+            <div>
+                <h1>Аторизация</h1>
+                <form onSubmit={this.submitHandler} className={'AuthForm'}>
+
+                    <Input label="Email"/>
+                    <Input
+                        label="Пароль"
+                        errorMessage={'Test'}
+                    />
+
+                    <Button
+                        type="success"
+                        onClick={this.loginHandler}
+                    >
+                        Войти
+                    </Button>
+                    <Button
+                        type="primary"
+                        onClick={this.registerHandler}
+                    >
+                        Зарегистрироваться
+                    </Button>
+                </form>
+            </div>
         </div>
         )
     }
